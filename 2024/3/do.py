2ad566b31da4001443ad6d96227f4e38f8b251ff
LAPTOP-M80SHDUR\lolo1 <90heedong@gmail.com>
2024-12-29 08:12:24 +0900

    2024 3 regex

---
+++ 2024/3/do.py
@@ -0,0 +1,25 @@
+import re
+
+f = open('data.txt', 'r')
+
+result = 0
+
+# mul(X,Y)
+
+for line in f.readlines():
+    while True:
+        x = re.search(r"mul\(\d{1,3},\d{1,3}\)", line)
+        if x:
+            x_str = x.group()
+
+            line = line.replace(x_str, '', 1)
+
+            x_str = x_str.replace("mul(", "")
+            x_str = x_str.replace(")", "")
+            x_nums = x_str.split(",")
+            result += int(x_nums[0]) * int(x_nums[1])
+        else:
+            break
+
+print(result)
+    
